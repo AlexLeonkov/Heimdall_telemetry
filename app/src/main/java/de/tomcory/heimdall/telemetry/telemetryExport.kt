b2312afbb16f806f5f06a219bbd86bb6ed390982
alexleonkov <aleonkov2001@gmail.com>
2023-11-28 15:14:39 +0100

    commented out not needed stuff

--- app/src/main/java/de/tomcory/heimdall/telemetry/telemetryExport.kt
+++ app/src/main/java/de/tomcory/heimdall/telemetry/telemetryExport.kt
@@ -1,27 +1,27 @@
-package de.tomcory.heimdall.telemetry
-
-import androidx.datastore.dataStore
-import de.tomcory.heimdall.persistence.database.dao.RequestDao
-import de.tomcory.heimdall.persistence.database.entity.Request
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.flow.collect
-
-
-class TelemetryExport(private val requestDao: RequestDao) {
-
-    fun exportDataToServer() {
-
-
-        CoroutineScope(Dispatchers.IO).launch {
-            requestDao.getAllObservable().collect { listOfRequests ->
-                listOfRequests.forEach { request ->
-                    println("Request: $request")
-                }
-            }
-        }
-    }
-}
-
-
+//package de.tomcory.heimdall.telemetry
+//
+//import androidx.datastore.dataStore
+//import de.tomcory.heimdall.persistence.database.dao.RequestDao
+//import de.tomcory.heimdall.persistence.database.entity.Request
+//import kotlinx.coroutines.CoroutineScope
+//import kotlinx.coroutines.Dispatchers
+//import kotlinx.coroutines.launch
+//import kotlinx.coroutines.flow.collect
+//
+//
+//class TelemetryExport(private val requestDao: RequestDao) {
+//
+//    fun exportDataToServer() {
+//
+//
+//        CoroutineScope(Dispatchers.IO).launch {
+//            requestDao.getAllObservable().collect { listOfRequests ->
+//                listOfRequests.forEach { request ->
+//                    println("Request: $request")
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//
