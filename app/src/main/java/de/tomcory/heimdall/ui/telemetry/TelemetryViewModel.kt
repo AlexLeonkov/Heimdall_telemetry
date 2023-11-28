913589001dc8cc8b40169cf80a1ade3d49309864
alexleonkov <aleonkov2001@gmail.com>
2023-11-28 21:08:02 +0100

    gson package added, handling @post

--- app/src/main/java/de/tomcory/heimdall/ui/telemetry/TelemetryViewModel.kt
+++ app/src/main/java/de/tomcory/heimdall/ui/telemetry/TelemetryViewModel.kt
@@ -4,35 +4,67 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.room.PrimaryKey
 import de.tomcory.heimdall.persistence.database.HeimdallDatabase
-import de.tomcory.heimdall.persistence.database.entity.Request
-import de.tomcory.heimdall.persistence.database.entity.Response
-import de.tomcory.heimdall.telemetry.TelemetryExport
+import de.tomcory.heimdall.persistence.database.entity.Request as EntityRequest
+import de.tomcory.heimdall.persistence.database.entity.Response as EntityResponse
+
+
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import okhttp3.ResponseBody
 import timber.log.Timber
+
+
+
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.Response
+
+
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class TelemetryViewModel() : ViewModel() {
 
     private val db: HeimdallDatabase? = HeimdallDatabase.instance
-    val requests: Flow<List<Request>>? = db?.requestDao?.getAllObservable()
-    val responses: Flow<List<Response>>? = db?.responseDao?.getAllObservable()
+    val requests: Flow<List<EntityRequest>>? = db?.requestDao?.getAllObservable()
+    val responses: Flow<List<EntityResponse>>? = db?.responseDao?.getAllObservable()
+
+    interface ApiService {
+        @POST("post")
+        suspend fun sendRequest(@Body requestData: EntityRequest): Response<Unit>
+    }
+
+    private val retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.x.x:your_port/") // Replace with your server IP and port
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+   private val apiService: ApiService = retrofit.create(ApiService::class.java)
+
+
+
 
 
     fun exportTelemetryData() {
         viewModelScope.launch {
-
-          requests?.collect { listOfRequests ->
-              Timber.d("not enteries")
-            listOfRequests.forEach { request ->
-
-                Timber.d(request.toString())
+            requests?.collect { listOfRequests ->
+                listOfRequests.forEach { request ->
+                    try {
+                        val response = apiService.sendRequest(request)
+                        if (response.isSuccessful) {
+                            Timber.d("Data sent successfully")
+                        } else {
+                            Timber.e("Error sending data")
+                        }
+                    } catch (e: Exception) {
+                        Timber.e(e, "Failed to send data")
+                    }
+                }
             }
-        }
-
-
-//            Timber.d("corrutin=ne")
-//            Timber.d(requests?.collect().toString() )
         }
         Timber.d("it worked")
     }
@@ -41,7 +73,7 @@ class TelemetryViewModel() : ViewModel() {
     fun createFakeData() {
         viewModelScope.launch {
             // Create a fake request
-            val fakeRequest = Request(
+            val fakeRequest = EntityRequest(
                     id = 1,
                     timestamp = 1633036800000,
                     reqResId = 101,
@@ -64,7 +96,7 @@ class TelemetryViewModel() : ViewModel() {
             db?.requestDao?.insert(fakeRequest)
 
             // Create a fake response corresponding to the fake request
-            val fakeResponse = Response(
+            val fakeResponse = EntityResponse(
                     id = 1,
                     timestamp = 1633036800000,
                     reqResId = 101,
